741de654d93a0f5485484293d66dbd08c3ddc8f5
Unknown <info@absolute-fx.com>
2019-09-18 23:42:39 +0200

    :bug: debug realty

--- controllers/realty.js
+++ controllers/realty.js
@@ -1,19 +1,22 @@
-const Realties = require('../repositories/Realties');
-const Librarycategories = require('../repositories/Librarycategories');
 
 exports.index = function(req, res, next) {
-    const status = 0;
-    const active = 1;
-    const media = 1;
-    const realtyId = req.query.id;
-    const project = 1;
-
     res.render('realty', {
         title: 'Imoges - Promotion immobilière',
         topNavActive: 'projects',
         breadcrumb: [
             {label: 'Accueil', link: '/'},
-            {label: 'Projets', link: '/projects'}
+            {label: 'Projets', link: '/projects'},
+            {label: realty.project.project_title, link: '/project?id=' + realty.projectId},
+            {label: realty.realty_title}
+        ],
+        css_paths: [
+            "/javascripts/plugins/slider.revolution/css/extralayers.css",
+            "/javascripts/plugins/slider.revolution/css/settings.css"
+        ],
+        js_paths:[
+            "/javascripts/plugins/slider.revolution/js/jquery.themepunch.tools.min.js",
+            "/javascripts/plugins/slider.revolution/js/jquery.themepunch.revolution.min.js",
+            "/javascripts/home_slider.js"
         ]
     });
 };
