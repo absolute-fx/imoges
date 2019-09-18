3b8706f58b902aaef67d0d82bc7e41a2bf5ed8f0
Unknown <info@absolute-fx.com>
2019-09-19 00:42:27 +0200

    :bug: debug realty

--- controllers/realty.js
+++ controllers/realty.js
@@ -16,29 +16,25 @@ exports.index = function(req, res, next) {
             const cat = "generic";
             const libraryMediaExt = "jpg";
 
-            Librarycategories.getAll({table, tblId, cat, libraryMediaExt}).then(librarycategories =>{
-                console.log(librarycategories);
-                res.render('realty', {
-                    title: 'Imoges - Promotion immobilière',
-                    topNavActive: 'projects',
-                    breadcrumb: [
-                        {label: 'Accueil', link: '/'},
-                        {label: 'Projets', link: '/projects'},
-                        {label: realty.project.project_title, link: '/project?id=' + realty.projectId},
-                        {label: realty.realty_title}
-                    ],
-                    css_paths: [
-                        "/javascripts/plugins/slider.revolution/css/extralayers.css",
-                        "/javascripts/plugins/slider.revolution/css/settings.css"
-                    ],
-                    js_paths:[
-                        "/javascripts/plugins/slider.revolution/js/jquery.themepunch.tools.min.js",
-                        "/javascripts/plugins/slider.revolution/js/jquery.themepunch.revolution.min.js",
-                        "/javascripts/home_slider.js"
-                    ],
-                    realty: realty,
-                    librarycategories: librarycategories
-                });
+            res.render('realty', {
+                title: 'Imoges - Promotion immobilière',
+                topNavActive: 'projects',
+                breadcrumb: [
+                    {label: 'Accueil', link: '/'},
+                    {label: 'Projets', link: '/projects'},
+                    {label: realty.project.project_title, link: '/project?id=' + realty.projectId},
+                    {label: realty.realty_title}
+                ],
+                css_paths: [
+                    "/javascripts/plugins/slider.revolution/css/extralayers.css",
+                    "/javascripts/plugins/slider.revolution/css/settings.css"
+                ],
+                js_paths:[
+                    "/javascripts/plugins/slider.revolution/js/jquery.themepunch.tools.min.js",
+                    "/javascripts/plugins/slider.revolution/js/jquery.themepunch.revolution.min.js",
+                    "/javascripts/home_slider.js"
+                ],
+                realty: realty
             });
         }
         else{
