9d651331c2d764ee48d2ed69bafac27a7c1b74a8
Unknown <info@absolute-fx.com>
2019-11-13 20:25:20 +0100

    :construction: compte

--- controllers/account.js
+++ controllers/account.js
@@ -17,5 +17,20 @@ exports.index = function(req, res, next) {
             ],
             user: req.session.user
         });
+    }else{
+        res.render('login', {
+            title: 'Espace client',
+            topNavActive: 'account',
+            breadcrumb: [
+                {label: 'Accueil', link: '/'},
+                {label: 'Connection'}
+            ],
+            css_paths: [
+
+            ],
+            js_paths:[
+                "/javascripts/auth.js"
+            ]
+        });
     }
 };
