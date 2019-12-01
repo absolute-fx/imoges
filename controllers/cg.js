7a961cb27e204e1b73e0b96b5f605f71453489cf
Unknown <info@absolute-fx.com>
2019-12-01 16:18:08 +0100

    ✨ Changement controller CG

--- controllers/cg.js
+++ controllers/cg.js
@@ -1,6 +1,7 @@
 exports.index = function(req, res) {
     res.render('cg', {
         title: "Aucun projet sur cette page",
+        page_description: "Détails sur les conditions générales d'utilisation du site web Imoges",
         topNavActive: '',
         breadcrumb: [
             {label: 'Accueil', link: '/'}
