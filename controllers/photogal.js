90d95988efb235aa30d2a06cf26619d63533986b
Manu <manu@absolute-fx.com>
2020-04-14 15:17:20 +0200

    ✨update galerie photo 2

--- controllers/photogal.js
+++ controllers/photogal.js
@@ -1,6 +1,6 @@
 exports.index = function(req, res, next) {
     let media = [];
-    for (let i=1; i<= 40; i++){
+    for (let i=1; i<= 55; i++){
         media.push({thumb: '/images/website_gal/thumb_' + i + '.jpg', fhd: '/images/website_gal/fhd_' + i + '.jpg'});
     }
     res.render('photogal', {
